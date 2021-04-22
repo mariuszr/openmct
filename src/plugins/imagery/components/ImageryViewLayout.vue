10454983167d1759a78d251b44eef560346bc40f
Mandlik, Nikhil K. (ARC-TI)[KBR Wyle Services, LLC] <nikhil.k.mandlik@nasa.gov>
2021-04-22 13:07:51 -0700

    fixed image tests

--- src/plugins/imagery/components/ImageryViewLayout.vue
+++ src/plugins/imagery/components/ImageryViewLayout.vue
@@ -381,7 +381,9 @@ export default {
             delete this.unsubscribe;
         }
 
-        this.imageContainerResizeObserver.disconnect();
+        if (this.imageContainerResizeObserver) {
+            this.imageContainerResizeObserver.disconnect();
+        }
 
         if (this.relatedTelemetry.hasRelatedTelemetry) {
             this.relatedTelemetry.destroy();
@@ -683,7 +685,7 @@ export default {
             window.clearInterval(this.durationTracker);
         },
         updateDuration() {
-            let currentTime = this.openmct.time.clock().currentValue();
+            let currentTime = this.openmct.time.clock() && this.openmct.time.clock().currentValue();
             this.numericDuration = currentTime - this.parsedSelectedTime;
         },
         resetAgeCSS() {
